54d9d3a8ccce7eb406618c4499ec60461f525e53
beenar-0 <dncastledn@gmail.com>
2022-12-18 17:32:55 +0300

    feat: add horizontal_keys array

--- modules/horizontal_keys.js
+++ modules/horizontal_keys.js
@@ -1,1 +1,150 @@
-export default []
+export default [
+    [
+        {
+        title: '1',
+        key: '1',
+        func: '1'
+        },
+        {
+            title: '2',
+            key: '2',
+            func: '2'
+        },
+        {
+            title: '3',
+            key: '3',
+            func: '3'
+        },
+        {
+            title: '/',
+            key: '/',
+            func: '/'
+        },
+        {
+            title: 'sin',
+            key: 'sin(',
+            func: 'Math.sin('
+        },
+        {
+            title: '(',
+            key: '(',
+            func: '('
+        },
+        {
+            title: ')',
+            key: ')',
+            func: ')'
+        }
+    ],
+    [
+        {
+            title: '4',
+            key: '4',
+            func: '4'
+        },
+        {
+            title: '5',
+            key: '5',
+            func: '5'
+        },
+        {
+            title: '6',
+            key: '6',
+            func: '6'
+        },
+        {
+            title: '*',
+            key: '*',
+            func: '*'
+        },
+        {
+            title: 'cos',
+            key: 'cos(',
+            func: 'Math.cos('
+        },
+        {
+            title: '𝝿',
+            key: '𝝿',
+            func: 'Math.PI'
+        },
+        {
+            title: '√ ',
+            key: '√(',
+            func: 'Math.sqrt('
+        }
+    ],
+    [
+        {
+            title: '7',
+            key: '7',
+            func: '7'
+        },
+        {
+            title: '8',
+            key: '8',
+            func: '8'
+        },
+        {
+            title: '9',
+            key: '9',
+            func: '9'
+        },
+        {
+            title: '+',
+            key: '+',
+            func: '+'
+        },
+        {
+            title: 'ln',
+            key: 'ln(',
+            func: 'Math.log('
+        },
+        {
+            title: 'e',
+            key: 'e',
+            func: 'Math.exp(1)'
+        },
+        {
+        title: '←',
+        key: '',
+        func: ''
+        }
+    ],
+    [
+        {
+            title: '.',
+            key: '.',
+            func: '.'
+        },
+        {
+            title: '0',
+            key: '0',
+            func: '0'
+        },
+        {
+            title: '^',
+            key: '^(',
+            func: '^('
+        },
+        {
+            title: '-',
+            key: '-',
+            func: '-'
+        },
+        {
+            title: '!',
+            key: '!',
+            func: '!'
+        },
+        {
+            title: 'C',
+            key: '',
+            func: ''
+        },
+        {
+            title: '=',
+            key: '',
+            func: ''
+        }
+    ]
+]
