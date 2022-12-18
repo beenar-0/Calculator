81757d86c5599d07e11826d16d4f7b49b491c5eb
beenar-0 <dncastledn@gmail.com>
2022-12-18 18:08:26 +0300

    feat: removed unused imports

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import Calculator from './modules/build.js'
-import horizontal_keys from "./modules/horizontal_keys.js";
 import vertical_keys from "./modules/vertical_keys.js";
-import render from './modules/render.js';
+
 
 const calculator = new Calculator()
 calculator.build(vertical_keys)
